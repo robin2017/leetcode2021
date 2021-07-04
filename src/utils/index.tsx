437ad548f74663d935aebd7b1a1183d55b233998
wangy202012 <1143027179@qq.com>
2021-07-04 15:16:05 +0800

    auto save

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,16 +1,22 @@
 import * as React from 'react';
 export const renderExpression = (
-  label: string,
-  express: string,
-  input: any
+  config:{
+    label: string,
+    express: string,
+    input: any,
+  },
+  aOptions:{
+    path:string
+  }
 ) => {
-  const inputStr = express.split('.')[0] // 对象为第一个单词
-  const func = new Function('' + inputStr, 'return ' + express);
+  const inputStr = config.express.split('.')[0] // 对象为第一个单词
+  const func = new Function('' + inputStr, 'return ' + config.express);
   return (
     <div>
-      <span style={{marginRight: '10px'}}>{label}:</span>
-      <span style={{marginRight: '10px',color: 'red'}}>{express}</span>
-      <span style={{marginRight: '10px',color: 'blue', fontWeight: 'bold'}}>{JSON.stringify(func(input))}</span>
+      <span style={{marginRight: '10px'}}>{config.label}:</span>
+      <span style={{marginRight: '10px',color: 'red'}}>{config.express}</span>
+      <span style={{marginRight: '10px',color: 'blue', fontWeight: 'bold'}}>{JSON.stringify(func(config.input))}</span>
+      {aOptions&&<a href={`https://github.com/robin2017/leetcode2021/blob/main/src/${aOptions.path}`} target='_blank' style={{fontSize: '12px', fontWeight: 'bold'}}>[code]</a>}
     </div>
   );
 };
